d52ec01d736e7b3ae3df901d6ad4d02450bba4a9
Huy Le <hule@atlassian.com>
2018-06-28 18:29:53 +1000

    Initial Commit

---
+++ ShineOn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShineOn
+//
+//  Created by Huy Le on 28/6/18.
+//  Copyright © 2018 Huy Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
